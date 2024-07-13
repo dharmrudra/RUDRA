f599b70a19f0793b28f333fc5f75accc7bf2041f
dharmrudra <165277525+dharmrudra@users.noreply.github.com>
2024-07-13 23:21:26 +0530

    afs

---
+++ Dev/main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_resource_group" "rgst" {
+   name     = "ramu45"
+  location = "westus"
+}
+resource "azurerm_resource_group" "rgs51" {
+   name     = "ramu56"
+  location = "westus"
+}
+resource "azurerm_resource_group" "rgs89" {
+   name     = "donfhjt"
+  location = "westus"
+}
+resource "azurerm_resource_group" "rgs87" {
+   name     = "sikanderhd678"
+  location = "westus"
+}
